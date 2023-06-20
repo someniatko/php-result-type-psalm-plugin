8c0100f4342327631065e15cd14b632d7f83a227
someniatko <illia.somov@bahnhof.net>
2023-06-20 13:06:21 +0300

    require_once the type provider because autoload seems not to work

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,6 +12,7 @@ final class Plugin implements PluginEntryPointInterface
 {
     public function __invoke(RegistrationInterface $registration, ?SimpleXMLElement $config = null): void
     {
+        require_once __DIR__ . '/ResultAllTypeProvider.php';
         $registration->registerHooksFromClass(ResultAllTypeProvider::class);
     }
 }
